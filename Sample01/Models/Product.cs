b7a4cbbbee9d2b313255e7f441e959b4c188b643
sandy0124@gmail.com <sandy0124@gmail.com>
2016-03-05 20:01:10 +0800

    Sample01Commit

--- Sample01/Models/Product.cs
+++ Sample01/Models/Product.cs
@@ -20,6 +20,7 @@ namespace Sample01.Models
         }
     
         public int ProductID { get; set; }
+
         public string Name { get; set; }
         public string ProductNumber { get; set; }
         public string Color { get; set; }
